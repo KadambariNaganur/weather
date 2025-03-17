d093349f12f4d9318f47e342ed4ca315c77b3b2b
KadambariNaganur <kadambari26312631@gmail.com>
2025-03-17 15:15:40 +0530

    add

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4%=yw9d4ott!qxyhvyda-ivv(@g@l(07t0=0y6=!39^^w#ude8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [weather-m6sl.onrender.com]
+ALLOWED_HOSTS = ["weather-m6sl.onrender.com"]
 
 
 # Application definition
